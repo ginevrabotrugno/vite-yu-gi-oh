22ef8e3b7996bbca69354d81ac1e908892393d76
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-09 16:44:19 +0200

    feat: add CharacterList

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import CharactersList from './components/CharactersList.vue';
 
 export default {
     name: 'App',
     components: {
         AppHeader,
+        CharactersList,
     }
 }
 </script>
 
 <template>
     <AppHeader />
+    <main>
+        <CharactersList />
+    </main>
 </template>
 
 <style lang="scss">
-@use './style/general.scss';
-
+@use './style/general.scss' as *;
 
 </style>
--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -5,9 +5,46 @@ export default {
 </script>
 
 <template>
+    <section>
+        <div class="container">
+            <div class="found">
+                Found 39 Cards
+            </div>
+            <div class="row">
 
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
+
+section {
+    padding: 50px 0;
+
+    .container {
+        background-color: #fff;
+        width: 80%;
+        margin: 0 auto;
+        padding-top: 50px;
+        // debug
+        min-height: 800px;
+
+        div {
+            width: 90%;
+            margin: 0 auto;
+        }
+
+        .found {
+            padding: 20px;
+            background-color: #212529;
+            color: #fff;
+            font-size: 18px;
+            font-weight: bolder;
+        }
+    }
+
+}
+
 
 </style>
